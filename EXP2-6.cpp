5b1a14d0ed419a8efb0a96c764401f7ee601895a
jdlllorente2 <47809894+jdlllorente2@users.noreply.github.com>
2019-05-13 23:22:42 +0800

    Add files via upload

---
+++ EXP2-6.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <conio.h>
+#include <cstlib>
+using namespace std;
+
+int main()
+{
+	int inputx, sum;
+	char num1, num2, A, B;
+	
+do
+{
+
+	cout << "Please enter the letter of the value of the interger x: \n";
+	cout << "A. 1 \n";
+	cout << "B. 2 \n";
+	cout << "C. Other values \n";
+	cin >> inputx;
+
+	
+		
+
+	}while(inputx >=0);
+	_getch();
+	return 0;
+}
+
